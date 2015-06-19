eabbb2d297f3cae474cb5d611b25c1ebaed332db
Cao Minh Trang <trangcm@gmail.com>
2015-06-19 17:50:06 +0700

    Fix bug Invalid parameter when calling encode function of libvpx
    Note: only vp8 works now

--- components/encoding/video_encoding_vp.cpp
+++ components/encoding/video_encoding_vp.cpp
@@ -1,12 +1,13 @@
 #include "video_encoding_vp.hpp"
 #include <stdio.h>
+#include <string.h>
 
 namespace oppvs
 {
 	int VPVideoEncoding::init(int width, int height)
 	{
 		vpx_codec_err_t res;
-		vpx_codec_iface_t *(*const codec_interface)() = &vpx_codec_vp9_cx;
+		vpx_codec_iface_t *(*const codec_interface)() = &vpx_codec_vp8_cx;
 
 		if (!vpx_img_alloc(&m_image, VPX_IMG_FMT_I420, width, height, 1)) {
     		printf("Failed to allocate image.\n");
@@ -34,26 +35,57 @@ namespace oppvs
 		return ERRS_ENCODING_OK;
 	}
 
-	int VPVideoEncoding::encode(uint8_t* frame, uint32_t length, uint8_t** encoded_frame)
+	int VPVideoEncoding::encode(PixelBuffer& pf, uint32_t* length, uint8_t** encoded_frame)
 	{
 		int frame_index = 1;
 	 	int flags = 0;
 	 	vpx_codec_iter_t iter = NULL;
 	  	const vpx_codec_cx_pkt_t *pkt = NULL;
 
-	  	memcpy(m_image.planes[0], frame, length);
+		if (updateImage(pf) < 0)
+			return -1;	  	
+
+
 	  	const vpx_codec_err_t res = vpx_codec_encode(&m_codec, &m_image, frame_index, 1,
 	                                               flags, VPX_DL_GOOD_QUALITY);
 	  
 	  	if (res != VPX_CODEC_OK)
 	    	printf("Failed to encode frame\n");
 
-	    int got_pkts = 0;
 	    while ((pkt = vpx_codec_get_cx_data(&m_codec, &iter)) != NULL) {
-	    	got_pkts = 1;
-	    	printf("Has packet\n");
+	    	switch (pkt->kind) {
+				case VPX_CODEC_CX_FRAME_PKT:
+					*length = pkt->data.frame.sz;
+					*encoded_frame = static_cast<uint8_t*>(pkt->data.frame.buf);
+					printf("encoded\n");
+					break;
+				default:
+					break;
+			}
 	    }
 		return 0;
+	}
+
+	int VPVideoEncoding::updateImage(PixelBuffer& pf)
+	{
+		uint16_t frame_width = pf.width[0];
+		uint16_t frame_height = pf.height[0];
+
+		int result = libyuv::ARGBToI420((const uint8_t*)pf.plane[0], frame_width*4,
+		      m_image.planes[0], m_image.stride[0],
+		      m_image.planes[1], m_image.stride[1],
+		      m_image.planes[2], m_image.stride[2],
+		      frame_width, frame_height);
+
+		/*m_image.w = m_image.d_w = frame_width;
+		m_image.h = m_image.d_h = frame_height;*/
+
+		m_image.x_chroma_shift = m_image.y_chroma_shift = 1;
+		m_image.planes[3] = NULL;
+
+		printf("%d %d %d %d\n", m_image.w, m_image.d_w, m_image.stride[1], m_image.stride[2]);
+
+		return result;
 	}
 
 	int VPVideoEncoding::release()
--- components/encoding/video_encoding_vp.hpp
+++ components/encoding/video_encoding_vp.hpp
@@ -5,6 +5,7 @@
 #include "vpx/vp8cx.h"
 #include "error.hpp"
 #include "datatypes.hpp"
+#include "libyuv.h"
 
 namespace oppvs
 {
@@ -13,12 +14,15 @@ namespace oppvs
 	{
 	public:
 		int init(int width, int height);
-		int encode(uint8_t* frame, uint32_t length, uint8_t** encoded_frame);	//Frame in YUV12 or I420 format
+		int encode(PixelBuffer& pf, uint32_t *length, uint8_t** encoded_frame);	//Frame in YUV12 or I420 format
+
 		int release();
 	private:
 		vpx_codec_ctx_t m_codec;
 		vpx_codec_enc_cfg_t m_configuration;
 		vpx_image_t m_image;
+
+		int updateImage(PixelBuffer& pf);
 	};
 }
 
