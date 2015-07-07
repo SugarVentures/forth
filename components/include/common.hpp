0991353b6b0a179a98e0d420d09819b13e562d13
Cao Minh Trang <trangcm@gmail.com>
2015-07-07 17:30:37 +0700

    Add ASSERT macro to replace the one in assert.h
    To show custom message

---
+++ components/include/common.hpp
@@ -0,0 +1,36 @@
+#ifndef OPPVS_COMMON_HPP
+#define OPPVS_COMMON_HPP
+
+#include <assert.h>
+#include <iostream>
+
+#define DEBUG 1
+
+
+#if( DEBUG == 1 )
+#define ASSERT( expression ) 				        \
+	if( expression!= true) {				\
+  		oppvs::localAssert(false, __FUNCTION__, __FILE__, __LINE__, #expression); \
+	}							\
+	else  oppvs::localAssert(true, __FUNCTION__, __FILE__, __LINE__, #expression); 	
+#elif( DEBUG == 0 )
+#define ASSERT( result )					\
+	void(0)
+#endif 
+
+namespace oppvs 
+{
+	bool localAssert(bool result, const char* function, const char* file, int line, const char* expression)
+	{
+		if (!result)
+		{
+			std::cout << expression;
+			std::cout << " Failed in file: " << file ; 
+			std::cout << " at function: " << function;
+  			std::cout << " at line: " << line << std::endl;
+		}
+		return result;
+	}
+}
+
+#endif
