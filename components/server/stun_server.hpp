3bf4e24197c622c6e7f6c3e4edd5faae7757234b
Cao Minh Trang <trangcm@gmail.com>
2015-07-13 11:36:09 +0700

    Add StunServerConfiguration
    Store configuration of stun server

--- components/server/stun_server.hpp
+++ components/server/stun_server.hpp
@@ -5,6 +5,41 @@
 
 namespace oppvs
 {
+	struct StunServerConfiguration
+	{
+		bool enabledPP;	//Primary IP, Primary Port
+		bool enabledPA;	//Primary IP, Alternate Port
+		bool enabledAP; //Alternate IP, Primary Port
+		bool enabledAA; //Alternate IP, Alternate Port
+
+		bool enabledMultithreadMode;	//1 thread - 1 socket
+
+		bool enabledTCP;
+		uint32_t maxConnections;
+
+		StunSocketAddress addressPP;
+		StunSocketAddress addressPA;
+		StunSocketAddress addressAP;
+		StunSocketAddress addressAA;
+
+		StunSocketAddress addressPrimaryAdvertised;
+		StunSocketAddress addressAlternateAdvertised;
+
+		bool enabledDosProtection;
+
+		StunServerConfiguration() :
+		{
+			enabledPP = false;
+			enabledPA = false;
+			enabledAP = false;
+			enabledAA = false;
+			enabledMultithreadMode = false;
+			enabledTCP = false;
+			maxConnections = 0;
+			enabledDosProtection = false;
+		}
+	};
+
 	class StunServer
 	{
 	public:
