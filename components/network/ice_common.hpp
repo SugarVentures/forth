b941932d8120dfccb3bf1733bc4b06e7b74487d7
Cao Minh Trang <trangcm@gmail.com>
2015-08-06 15:22:33 +0700

    Add IceServerInfo object
    Store information of Turn/Stun

---
+++ components/network/ice_common.hpp
@@ -0,0 +1,25 @@
+#ifndef OPPVS_ICE_COMMON_HPP
+#define OPPVS_ICE_COMMON_HPP
+
+#include "datatypes.hpp"
+#include <string>
+
+namespace oppvs {
+	struct IceServerInfo
+	{
+		std::string serverAddress;
+		uint16_t port;
+		std::string username;
+		std::string password;
+
+		IceServerInfo(std::string addr, uint16_t p, std::string un = "", std::string pwd = "")
+		{
+			serverAddress = addr;
+			port = p;
+			username = un;
+			password = pwd;
+		}
+	};
+} // oppvs
+
+#endif // OPPVS_ICE_COMMON_HPP
