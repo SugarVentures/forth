0726d3a291a9e8be0e33680b2f4c012f3eecdc51
Cao Minh Trang <trangcm@gmail.com>
2015-11-03 18:08:48 +0700

    Add audio_util.h to include macros for cross compiling osx & ios

---
+++ components/audio/audio_util.h
@@ -0,0 +1,15 @@
+#ifndef OPPVS_AUDIO_UTIL_H
+#define OPPVS_AUDIO_UTIL_H
+
+#ifdef FORTH_IOS
+#import <CoreFoundation/CoreFoundation.h>
+
+typedef UInt32 AudioDeviceID;
+#define kAudioUnitSubType_HALOutput kAudioUnitType_Output;
+#define kAudioUnitSubType_DefaultOutput kAudioUnitSubType_GenericOutput;
+#endif
+
+
+#endif
+
+
