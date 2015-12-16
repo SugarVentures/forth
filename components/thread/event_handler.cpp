7bc961c389be6b24e96d00255e7a12cf27262957
Cao Minh Trang <trangcm@gmail.com>
2015-12-16 17:27:39 +0700

    Update EventHandler to manage events at different threads

--- components/thread/event_handler.cpp
+++ components/thread/event_handler.cpp
@@ -1,7 +1,7 @@
 #include "event_handler.hpp"
 
 namespace oppvs {
-	EventHandler::EventHandler()
+	EventHandler::EventHandler(): m_numEvents(0)
 	{
 		p_thread = new Thread(EventHandler::run, this);
 	}
@@ -20,14 +20,60 @@ namespace oppvs {
 
 	void EventHandler::runImpl()
 	{
-		std::unique_lock<std::mutex> lk(m_mutex);
-		m_conditionVariable.wait(lk, []{return m_hasMessage;});
-		printf("Wake up\n");
+		for (;;)
+		{
+			std::unique_lock<std::mutex> lk(m_mutex);
+			m_conditionVariable.wait(lk, [this]{return (m_activeEventsList.size() > 0);});
+			printf("Wake up\n");
+			if (processSignal() == 1)
+				break;
+		}
 	}	
 
 	void EventHandler::start()
 	{
 	    p_thread->create();
-	    p_thread->join();
+	}
+
+	void EventHandler::stop()
+	{
+		ActiveEvent event;
+		event.signal = EVENT_SIGNAL_STOP;
+		m_activeEventsList.push(event);
+		m_conditionVariable.notify_one();
+	}
+
+	void EventHandler::registerEvent(callbackFunctionType fnc)
+	{
+		cbFunction = fnc;
+		int localNumEvents = m_numEvents++;
+		printf("current id: %d\n", localNumEvents);
+		Event event;
+		event.eventId = localNumEvents;
+		event.cbFunction = fnc;
+		{
+			std::lock_guard<std::mutex> lk(m_writeMutex);
+			m_eventsList.push_back(event);
+		}
+	}
+
+	void EventHandler::sendSignal()
+	{
+		printf("Receive signal\n");
+		ActiveEvent event;
+		m_activeEventsList.push(event);
+		m_conditionVariable.notify_one();
+	}
+
+	int EventHandler::processSignal()
+	{
+		while (m_activeEventsList.size() > 0)
+		{
+			std::shared_ptr<ActiveEvent> event = m_activeEventsList.pop();
+			printf("Hello\n");
+			if (event->signal == EVENT_SIGNAL_STOP)
+				return 1;
+		}
+		return 0;
 	}
 } // oppvs
--- components/thread/event_handler.hpp
+++ components/thread/event_handler.hpp
@@ -6,29 +6,55 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
+#include <vector>
+#include <atomic>
 
 #include "thread.hpp"
+#include "tsqueue.hpp"
 
 namespace oppvs {
 
+	const int EVENT_SIGNAL_STOP = 0x0000;
 	const int MESSAGE_CACHE_CLEAN = 0x0001;
-	bool m_hasMessage = false;
+
+	typedef std::function<int(const std::string&)> callbackFunctionType;
+
+	struct Event
+	{
+		int eventId;
+		callbackFunctionType cbFunction;
+	};
+
+	struct ActiveEvent
+	{
+		int signal;
+	};
 	
 	class EventHandler {
 	private:
 		Thread* p_thread;
 		std::mutex m_mutex;
+		std::mutex m_writeMutex;
 		std::condition_variable m_conditionVariable;
 
-		
+		callbackFunctionType cbFunction;
+		std::vector<Event> m_eventsList;
+		tsqueue<ActiveEvent> m_activeEventsList;
 
 		static void* run(void* object);
 		void runImpl();
+
+		std::atomic<int> m_numEvents;
+
+		int processSignal();
+
 	public:
 		EventHandler();
 		~EventHandler();
-		void registerEvent();
+		void registerEvent(callbackFunctionType);
+		void sendSignal();
 		void start();
+		void stop();
 	};
 } // oppvs
 
