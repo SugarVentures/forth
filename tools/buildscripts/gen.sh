a4a81b22a34d70826eb26b2938aebb1653840d9e
Cao Minh Trang <trangcm@gmail.com>
2016-01-18 17:40:04 +0700

    Create automated script to generate build settings for all libs in OS X

--- tools/buildscripts/gen.sh
+++ tools/buildscripts/gen.sh
@@ -3,9 +3,17 @@
 HERE=$(dirname $0)
 ROOT_DIR=${HERE}/../..
 
+OS_VAR="mac"
+OUTPUT_DIR="mac"
+
+echo $1
+
+if [ "$1" != "" ]; then
+	OS_VAR=$1
+	OUTPUT_DIR=$1
+fi
+
 #Go to components folders
-OUTPUT_DIR="ios"
-OS_VAR="ios"
 
 cd "${ROOT_DIR}/components/audio"
 gyp audio_engine.gyp --depth=. -f xcode -DOS=${OS_VAR} --generator-output=${OUTPUT_DIR}
