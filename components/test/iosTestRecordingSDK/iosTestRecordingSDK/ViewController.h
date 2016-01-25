41f848261407cb4ad40dd7160a440c928f93a56d
Cao Minh Trang <trangcm@gmail.com>
2016-01-25 18:09:02 +0700

    Create ios app to test recording engine

---
+++ components/test/iosTestRecordingSDK/iosTestRecordingSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosTestRecordingSDK
+//
+//  Created by Cao Minh Trang on 1/25/16.
+//  Copyright © 2016 Cao Minh Trang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ components/test/iosTestRecordingSDK/iosTestRecordingSDK/ViewController.mm
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  iosTestRecordingSDK
+//
+//  Created by Cao Minh Trang on 1/25/16.
+//  Copyright © 2016 Cao Minh Trang. All rights reserved.
+//
+
+#import "ViewController.h"
+#include "ios_video_capture.hpp"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
