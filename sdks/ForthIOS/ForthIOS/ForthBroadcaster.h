e72ded7c354be62978f1ed7e38150ff567fd87a1
Cao Minh Trang <trangcm@gmail.com>
2016-01-27 14:44:35 +0700

    Add ForthBroadcaster Interface to ForthIOS sdk

---
+++ sdks/ForthIOS/ForthIOS/ForthBroadcaster.h
@@ -0,0 +1,19 @@
+//
+//  ForthBroadcaster.h
+//  ForthIOS
+//
+//  Created by Cao Minh Trang on 1/27/16.
+//  Copyright © 2016 Cao Minh Trang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ForthBroadcaster : NSObject
+{
+    
+}
+
+- (id) init: (NSString*)serverAddress;
+- (id) init;
+
+@end
